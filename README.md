b9085de50901be8bddbc314c76a8582fcb8d0099
VincenzoLivigni <enzolivigni13@gmail.com>
2026-02-16 20:51:58 +0100

    Milestone 1

---
+++ README.md
@@ -0,0 +1,10 @@
+📌 Milestone 1: Recuperare e visualizzare i dati
+✅ - 1 Effettua una chiamata API a http://localhost:3333/politicians
+
+✅ - 2 Salva la risposta in uno stato React (useState).
+
+✅ - 3 Mostra i politici in una lista di card, visualizzando almeno le seguenti proprietà:
+        Nome (name)
+        Immagine (image)
+        Posizione (position)
+        Breve biografia (biography)
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,51 @@
+import { useEffect, useState } from "react"
+
 function App() {
+
+  const [politicians, setPoliticians] = useState([])
+
+  const loadPoliticians = async () => {
+    const res = await fetch("http://localhost:3333/politicians")
+    const data = await res.json()
+    setPoliticians(data)
+  }
+
+  useEffect(() => {
+    loadPoliticians()
+  }, [])
 
   return (
     <>
+      <header>
+        <h1 className="text-center py-3">List of politicians</h1>
+      </header>
 
+      <main className="bg-light">
+        <div className="container">
+          <div className="row row-cols-4">
+            {
+              politicians.map((p) => (
+                <div key={p.id} className="col d-flex">
+                  <div className="card my-5 flex-fill rounded-4">
+                    <img src={p.image} className="rounded-4 rounded-bottom-0" />
+                    <div className="card-body">
+                      <p>
+                        <strong>Name:</strong> <span>{p.name}</span>
+                      </p>
+                      <p>
+                        <strong>Position:</strong> <span>{p.position}</span>
+                      </p>
+                      <p>
+                        <strong>Biography:</strong> <span>{p.biography}</span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+      </main>
     </>
   )
 }
